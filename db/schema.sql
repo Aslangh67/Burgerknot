14a9982d4ec85d6df75fdc98a9a7b61e2813139b
Mr. A <ag.perch@gmail.com>
2020-01-16 16:03:52 -0800

    kinda working

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+### Schema
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+-- changed sleppy to eaten
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('plain');
+INSERT INTO burgers (name) VALUES ('chees burger');
+INSERT INTO burgers (name, eaten) VALUES ('ranch burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('spicy burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('double burger', true);
+INSERT INTO burgers (name) VALUES ('veggie burger');
